d725205ebc786be2101322ff20ddacd794849d9d
Tanachuns <tanarak.chuns@gmail.com>
2025-02-07 10:09:57 +0700

    add internal

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Tanachuns/teebaan-payment/internal/app"
+
+func main() {
+	app.Start()
+}
--- internal/server.go
+++ internal/server.go
@@ -1,14 +1,16 @@
-package main
+package app
 
 import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/time/rate"
 )
 
-func main() {
+func Start() {
 	e := echo.New()
-
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(rate.Limit(1))))
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
